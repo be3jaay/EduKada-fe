66643ffdd140e56fa265e73acef877257c240dae
Lizter <lizters74@gmail.com>
2024-12-09 02:10:42 +0800

    changes header bg

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,10 +4,10 @@ import { HeaderLinks } from "@/constant/Link";
 
 export const Header: React.FC = () => {
   return (
-    <div className="w-full px-20 py-5 bg-primary rounded-md shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[9.7px] border border-[rgba(35,17,35,0.67)] fixed ">
+    <div className="w-full px-20 py-5 bg-[rgba(0,0,0,0.5)] backdrop-blur-lg border border-[rgba(255,255,255,0.2)] rounded-lg shadow-lg fixed ">
       <div className="w-full flex flex-wrap justify-center sm:justify-between ">
         <div className="flex-wrap justify-between hidden md:flex">
-          <Button className="hover:bg-primary/90">LOGO</Button>
+          <Button className="bg-primary hover:bg-primary/90">LOGO</Button>
         </div>
 
         <div className="flex gap-10 justify-center items-center ">
@@ -22,7 +22,7 @@ export const Header: React.FC = () => {
         </div>
 
         <div className="flex-wrap justify-between hidden sm:flex ">
-          <Button className=" hover:bg-primary/90">SIGN IN</Button>
+          <Button className=" bg-primary hover:bg-primary/90">SIGN IN</Button>
         </div>
       </div>
     </div>
